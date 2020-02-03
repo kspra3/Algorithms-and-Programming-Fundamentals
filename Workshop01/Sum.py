c7317d986426c64caa8f723194a6040d06c0055b
Sharsindra Pratheen <56106960+kspra3@users.noreply.github.com>
2020-02-04 04:22:01 +0800

    Add files via upload

---
+++ Workshop01/Sum.py
@@ -0,0 +1,11 @@
+# K. Sharsindra Pratheen
+# Advanced Computer Science
+# Monash University
+
+# Add two numbers and display result on the screen.
+# num1 and num2 are obtained from the user.
+num1 = int(input("please enter the first number: "))
+num2 = int(input("please enter the second number: "))
+
+sum1 = num1 + num2
+print(sum1)
