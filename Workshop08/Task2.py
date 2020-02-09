cf1e5ba8cc1868f72ce40813819f8bb0ebfce7fc
Sharsindra Pratheen <56106960+kspra3@users.noreply.github.com>
2020-02-10 05:18:13 +0800

    Add files via upload

---
+++ Workshop08/Task2.py
@@ -0,0 +1,23 @@
+# K. Sharsindra Pratheen
+# 25636626
+# Advanced Computer Science, Monash University
+
+fileName = input ("Please enter the filename : ")
+list = []
+data = open(fileName)
+
+for line in data :
+    temp = line.strip("\n") # Remove the new line character at the end.
+    temp = temp.split("\t") # Split the line after the tab.
+    temp[1] = temp[1].split(',') # Split what’s after the tab.
+    list .append(temp)
+
+data.close()
+pairedList =[]
+for row in list:
+    for element in range (1 , len(row)):
+        pair = [row[0],row[element]]
+        pairedList.append(pair)
+
+for row in pairedList:
+    print(row[0],row[1])
